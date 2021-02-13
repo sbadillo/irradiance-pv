6b007e5a95b8389bd3873d2ed92a6a446ded4167
sergiob <sbadillo@gmail.com>
2021-02-13 13:23:58 +0100

    setup.py points to README.md

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("irradiance_pv/README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="irradiance_pv",
-    version=1.1,
+    version=1.2,
     description="Calculate in plane irradiance for a surface",
     long_description=long_description,
     long_description_content_type="text/markdown",
